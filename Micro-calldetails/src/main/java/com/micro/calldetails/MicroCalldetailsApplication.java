c6fcdf7bb4c4080e025880e377683dbe09dcc72d
michaelselvan <michaelselvan2005@gmail.com>
2020-12-03 16:35:18 +0530

    modied for testing purpose

--- Micro-calldetails/src/main/java/com/micro/calldetails/MicroCalldetailsApplication.java
+++ Micro-calldetails/src/main/java/com/micro/calldetails/MicroCalldetailsApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+
+
 public class MicroCalldetailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroCalldetailsApplication.class, args);
+		System.out.println("this is newly added line by michael in STS workspace.........");
+	
 	}
 
 }
